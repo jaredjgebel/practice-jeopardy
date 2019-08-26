1700f210ccbc77a7668a6db9f23ca36e7f5bed5c
Jared Gebel <jaredjgebel@gmail.com>
2019-08-26 19:29:15 -0400

    New build

---
+++ client/build/precache-manifest.43f7920589c07211847bbdb772c1e786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8f972dc27afe60774991b036fb2ed7b",
+    "url": "/static/media/beige-paper.c8f972dc.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5178eb0f0ce1494e5397",
+    "url": "/static/js/main.5178eb0f.chunk.js"
+  },
+  {
+    "revision": "3342ad4aedf6eee1033b",
+    "url": "/static/js/2.3342ad4a.chunk.js"
+  },
+  {
+    "revision": "5178eb0f0ce1494e5397",
+    "url": "/static/css/main.e6a9f748.chunk.css"
+  },
+  {
+    "revision": "2e6effe6232d5a78a932001b765fdc75",
+    "url": "/index.html"
+  }
+];
